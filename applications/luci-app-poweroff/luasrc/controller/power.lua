49bc263f5231f41869b8f7a77002ba047405fe78
cc <cc>
2024-06-23 16:15:11 +0800

    关机

---
+++ applications/luci-app-poweroff/luasrc/controller/power.lua
@@ -0,0 +1,20 @@
+
+module("luci.controller.poweroff", package.seeall)
+
+
+function index() 
+    local e = entry({"admin","system","power"},template("poweroffdevice/poweroffdevice"), _("PowerOff"), 92)
+    e.dependent=false
+    e.acl_depends = { "luci-app-poweroff" }
+	entry({"admin","system","poweroff","poweroff"},post("action_poweroff"))
+	entry({"admin","system","poweroff","reboot"},post("action_reboot"))
+end
+
+function action_poweroff()
+    luci.sys.exec("/sbin/poweroff" )
+end
+
+
+function action_reboot()
+    luci.sys.exec("/sbin/reboot" )
+end
