73294cddf2b6529cf64d0f5aeab38fbaab13db20
cc <cc>
2024-06-24 11:34:18 +0800

    up

--- applications/luci-app-poweroff/luasrc/controller/poweroff.lua
+++ applications/luci-app-poweroff/luasrc/controller/poweroff.lua
@@ -5,6 +5,7 @@ module("luci.controller.poweroff", package.seeall)
 function index() 
     local e = entry({"admin","system","poweroff"},template("poweroffdevice/poweroffdevice"), _("PowerOff"), 92)
     e.dependent=false
+    e.i18n="poweroff"
     e.acl_depends = { "luci-app-poweroff" }
 	entry({"admin","system","poweroff","poweroff"},post("action_poweroff"))
 	entry({"admin","system","poweroff","reboot"},post("action_reboot"))
