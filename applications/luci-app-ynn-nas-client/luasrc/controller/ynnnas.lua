53210736aafdb0bca5a1562165a2e212800c63c6
cc <cc>
2024-07-23 15:58:21 +0800

    up

--- applications/luci-app-ynn-nas-client/luasrc/controller/ynnnas.lua
+++ applications/luci-app-ynn-nas-client/luasrc/controller/ynnnas.lua
@@ -15,7 +15,7 @@ function start()
 end
 
 function check()
-    local running=(luci.sys.call("[ ` ps -w | grep ynn-nas-client| grep -v grep | awk '{print $1}' | wc -l` -gt 0 ] > /dev/null") == 0)
+    local running=(luci.sys.call("[ ` ps -w | grep /usr/sbin/ynn-nas-client| grep -v grep | awk '{print $1}' | wc -l` -gt 0 ] > /dev/null") == 0)
 	if running then
 		 luci.http.write("1")
 	else
